d15c3b8b834531768383dcda0a7f9af3734cabee
heoyoon <parechae@gmail.com>
2024-10-08 21:46:32 +0900

    test : A* 알고리즘 몬스터 프레임 테스트
    4060 i7 13세대 16기가 메모리 노트북에서 100프레임 방어 성공

--- URPRagProjectSingle/Assets/Characters/Monster/TempMonster.cs
+++ URPRagProjectSingle/Assets/Characters/Monster/TempMonster.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using PlayerDefines;
 using PlayerDefines.Stat;
+using Unity.VisualScripting;
+using System.Linq;
 
 public class TempMonster : MonoBehaviour
 {
     // Start is called before the first frame update
     [SerializeField]Stats monsterStat;
+    LinkedList<Node> path = new LinkedList<Node>();
+    Node targetNode
+    {
+        get { return Player.Instance.playerLevelInfo.stat.standingNode; }
+    }
     public Node CurrentNode
     {
         get { return monsterStat.standingNode; }
@@ -23,5 +30,25 @@ public class TempMonster : MonoBehaviour
         monsterStat = new Stats(GridManager.GetInstance().PositionToNode(transform.position),30,10,3,3,10,1);
         transform.position = new Vector3(monsterStat.standingNode.nodeCenterPosition.x, monsterStat.standingNode.nodeFloor+1.5f, monsterStat.standingNode.nodeCenterPosition.y);
         Debug.Log(monsterStat.standingNode.nodeCenterPosition);
+        path = GridManager.GetInstance().PathFinding(monsterStat.standingNode.nodeCenterPosition, targetNode.nodeCenterPosition);
+    }
+    private void Update()
+    {
+        if (path.Last<Node>() != targetNode)
+        {
+            Debug.Log("플레이어 탐색중");
+            path = GridManager.GetInstance().PathFinding(monsterStat.standingNode.nodeCenterPosition, targetNode.nodeCenterPosition);
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        if (path == null) return;
+        if (path.Count < 0) return;
+        Node[] tempNodeArray = path.ToArray();
+        for (int i = 0; i < path.Count; i++)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawCube(new Vector3(tempNodeArray[i].nodeCenterPosition.x, transform.position.y, tempNodeArray[i].nodeCenterPosition.y), Vector3.one);
+        }
     }
 }
