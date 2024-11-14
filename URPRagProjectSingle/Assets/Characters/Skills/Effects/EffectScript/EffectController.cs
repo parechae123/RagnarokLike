7d523540eedd29f8566ad207fd9f347d3176cb1e
heoyoon <parechae@gmail.com>
2024-11-14 14:10:47 +0900

    feat : 이펙트 큐 추가 중간커밋

--- URPRagProjectSingle/Assets/Characters/Skills/Effects/EffectScript/EffectController.cs
+++ URPRagProjectSingle/Assets/Characters/Skills/Effects/EffectScript/EffectController.cs
@@ -6,15 +6,38 @@ using UnityEngine;
 public class EffectController : MonoBehaviour
 {
     public GameObject prefab;
+    public Queue<Effects> readyQueue = new Queue<Effects>();
     public Effects[] effect= new Effects[1];
-    public void playOrder(Vector3 position)
+    public void PlayOrder(Vector3 position)
     {
         //QueueStack형태로 
+        
     }
-    
+    public void CreateEffect()
+    {
+        
+    }
+    public IEnumerator SkillTimer(Vector3 position)
+    {
+        if (readyQueue.Count > 0 )
+        {
+            //여기서 기다리고
+            readyQueue.TryDequeue(out Effects result);
+            yield return new WaitForSeconds(result.particle.main.duration);
+            result.particle.gameObject.SetActive(false);
+            readyQueue.Enqueue(result);
+            yield break;
+        }
+        else
+        {
+            CreateEffect();
+            StartCoroutine(SkillTimer(position));
+            yield break;
+        }
+    }
 }
 [System.Serializable]
-public class Effects
+public struct Effects
 {
     public ParticleSystem particle;
     public GameObject decal;
@@ -28,12 +51,11 @@ public class Effects
     /// <summary>
     /// 이미 재생중이라 실패 시 false를 리턴
     /// </summary>
-    /// <param name="positions">쎾쓰</param>
+    /// <param name="positions"></param>
     /// <returns></returns>
     public void Play(Vector3 positions)
     {
         particle.transform.position = positions;
-        decal.transform.position = positions;
         particle.Play();
         decal.SetActive(true);
     }
