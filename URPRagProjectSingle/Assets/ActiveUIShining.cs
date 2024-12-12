6f957de5363bdfaf7c17cffc2fa6454478d94f29
heoyoon <parechae@gmail.com>
2024-12-13 03:22:25 +0900

    feat : UI 강조 쉐이더와 퀘스트 연출추가

---
+++ URPRagProjectSingle/Assets/ActiveUIShining.cs
@@ -0,0 +1,27 @@
+using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class ActiveUIShining : MonoBehaviour
+{
+    [SerializeField] private Material material;
+    float timer;
+
+    private void OnEnable()
+    {
+        material.SetFloat("_Timer", 0);
+        ((RectTransform)transform).sizeDelta= new Vector3(400, 100, 1);
+        ((RectTransform)transform).DOSizeDelta(new Vector3(400,250,1), 0.3f);
+        timer = -0.3f;
+    }
+    private void Update()
+    {
+        if (timer > material.GetFloat("_CycleTime")) return;
+        timer += Time.deltaTime;
+        material.SetFloat("_Timer", timer);
+    }
+}
